18fae7a8ee30eadeaba3b359d30c7fe12cf675bc
YHSPY-SURFACE <yhorg@hotmail.com>
2017-02-23 09:04:12 +0800

    Update code.

--- src/ballpen-plugin-cache.js
+++ src/ballpen-plugin-cache.js
@@ -1,5 +1,26 @@
+import Localforage from 'localforage';
+
 class BallpenPluginCache {
-	
+	static init() {
+        return {
+            // Data API
+            set: Localforage.setItem.bind(Localforage),
+            get: Localforage.getItem.bind(Localforage),
+            remove: Localforage.removeItem.bind(Localforage),
+            length: Localforage.length.bind(Localforage),
+            clear: Localforage.clear.bind(Localforage),
+            key: Localforage.key.bind(Localforage),
+            keys: Localforage.keys.bind(Localforage),
+            iterate: Localforage.iterate.bind(Localforage),
+            // Settings API
+            setDriver: Localforage.setDriver.bind(Localforage),
+            config: Localforage.config.bind(Localforage),
+            // Custom Driver API
+            defineDriver: Localforage.defineDriver.bind(Localforage),
+            driver: Localforage.driver.bind(Localforage),
+            supports: Localforage.supports.bind(Localforage)
+        };
+    };
 }
 
 export default BallpenPluginCache;
