0d11964c292710bef7859f8030b24fdffed86e96
YHSPY-SURFACE <yhorg@hotmail.com>
2017-02-23 09:23:26 +0800

    Update wrapper structure.

--- src/ballpen-plugin-cache.js
+++ src/ballpen-plugin-cache.js
@@ -1,25 +1,56 @@
 import Localforage from 'localforage';
 
 class BallpenPluginCache {
-	static init() {
-        return {
-            // Data API
-            set: Localforage.setItem.bind(Localforage),
-            get: Localforage.getItem.bind(Localforage),
-            remove: Localforage.removeItem.bind(Localforage),
-            length: Localforage.length.bind(Localforage),
-            clear: Localforage.clear.bind(Localforage),
-            key: Localforage.key.bind(Localforage),
-            keys: Localforage.keys.bind(Localforage),
-            iterate: Localforage.iterate.bind(Localforage),
-            // Settings API
-            setDriver: Localforage.setDriver.bind(Localforage),
-            config: Localforage.config.bind(Localforage),
-            // Custom Driver API
-            defineDriver: Localforage.defineDriver.bind(Localforage),
-            driver: Localforage.driver.bind(Localforage),
-            supports: Localforage.supports.bind(Localforage)
-        };
+    static set(...args) {
+        return Localforage.setItem(...args);
+    };
+
+    static get(...args) {
+        return Localforage.getItem(...args);
+    };
+
+    static remove(...args) {
+        return Localforage.remove(...args);
+    };
+
+    static length(...args) {
+        return Localforage.length(...args);
+    };
+
+    static clear(...args) {
+        return Localforage.clear(...args);
+    };
+
+    static key(...args) {
+        return Localforage.key(...args);
+    };
+
+    static keys(...args) {
+        return Localforage.keys(...args);
+    };
+
+    static iterate(...args) {
+        return Localforage.iterate(...args);
+    };
+
+    static setDriver(...args) {
+        return Localforage.setDriver(...args);
+    };
+
+    static config(...args) {
+        return Localforage.config(...args);
+    };
+
+    static defineDriver(...args) {
+        return Localforage.defineDriver(...args);
+    };
+
+    static driver(...args) {
+        return Localforage.setDriver(...args);
+    };
+
+    static supports(...args) {
+        return Localforage.supports(...args);
     };
 }
 
